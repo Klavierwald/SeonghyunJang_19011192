d2f9ba730d83213f7110e31989199876f14394cd
UNISUM <UNISUM@DESKTOP-JPQO6FJ>
2023-06-03 23:22:22 +0900

    update 23.06.03. 11:22

--- ECI2ECEF_DCM.m
+++ ECI2ECEF_DCM.m
@@ -3,20 +3,20 @@
 
 function DCM = ECI2ECEF_DCM(time)
     % input: time in [yyyy mm dd hh mm ss] vector format
-    %   where GMST, ECEF is calculated under the time = GMST + 0 (GMST from GNSS data)
-    %   where UTC,  ECEF is calculated under the time = GMST + UTC (at Ground Station, UTC from longitude of Ground Station)
+    %   where UTC, ECEF is calculated under the time = UTC + 0 (UTC from GNSS data)
+    %   where LT,  ECEF is calculated under the time = UTC + LT (at Ground Station, LT = UTC+n from longitude of Ground Station)
     % output: DCM, Coordinate Transform Matrix
-    % R_UTC  indicates Rotation Transform from Original co. to Rotated co.(Axis Transform)
-    % R_UTC' indicates Coordinate Transform from Rotated co. to Original co. by the above definition
+    % R_LT  indicates Rotation Transform from Original co. to Rotated co.(Axis Transform)
+    % R_LT' indicates Coordinate Transform from Rotated co. to Original co. by the above definition
     
     time2long = siderealTime(juliandate(datetime(time)));
     time2long = time2long*pi/180;
     
-    R_UTC = [cos(time2long) -sin(time2long)  0.0; ...
+    R_LT  = [cos(time2long) -sin(time2long)  0.0; ...
              sin(time2long)  cos(time2long)  0.0; ...
              0.0             0.0             1.0];
 
-    DCM = R_UTC';
-    % R_UTC  is Rotation Matrix from ECEF to ECI
-    % R_UTC' is Coordinate Transform Matrix from ECI to ECEF
+    DCM = R_LT';
+    % R_LT  is Rotation Matrix from ECEF to ECI
+    % R_LT' is Coordinate Transform Matrix from ECI to ECEF
 end
